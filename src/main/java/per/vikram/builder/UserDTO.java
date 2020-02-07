faf876081a6f489a4cd55d2aee05f8c4e5199b66
vikramthakur <vikramthakur@nbnco.com.au>
2020-02-07 17:54:16 +1100

    Builder Pattern

---
+++ src/main/java/per/vikram/builder/UserDTO.java
@@ -0,0 +1,7 @@
+package per.vikram.builder;
+
+public interface UserDTO {
+    public String getName();
+    public String getAddress();
+    public int getAge();
+}
---
+++ src/main/java/per/vikram/builder/UserDtoConcrete.java
@@ -0,0 +1,32 @@
+package per.vikram.builder;
+
+public class UserDtoConcrete implements UserDTO {
+    private String name;
+    private String address;
+    private int age;
+
+    public UserDtoConcrete(String name,String address,int age){
+        this.age=age;
+        this.address = address;
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getAddress() {
+        return this.address;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String toString() {
+        return this.name+" lives at "+this.address+" is "+this.age+" years old";
+    }
+}
